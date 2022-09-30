d1a2298b4e67efb8d19bbbf38450862e73ba662a
Cheng Lim <wenyong.lim@icloud.com>
2022-09-30 14:15:43 +0100

    Init function

--- main.js
+++ main.js
@@ -4,15 +4,19 @@ const btn = document.querySelector('.btn');
 const footer = document.querySelector('.footer');
 const navLogo = document.getElementById('#logo');
 
-anime({
-  targets: '#logo',
-  translateX: 250,
-  rotate: '1turn',
-  backgroundColor: '#FFF',
-  duration: 3000,
-});
+const init = () => {
+  anime({
+    targets: '#logo',
+    translateX: 250,
+    rotate: '1turn',
+    backgroundColor: '#FFF',
+    duration: 3000,
+  });
 
-anime({
-  targets: '.nav-links',
-  translateX: -250,
-});
+  anime({
+    targets: '.nav-links',
+    translateX: -250,
+  });
+};
+
+init();
