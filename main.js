b6cb45a1f749cd87855376fa0595bb1d23797d14
Cheng Lim <wenyong.lim@icloud.com>
2022-10-10 17:30:15 +0100

    Import and css

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import anime from './node_modules/animejs/lib/anime.es.js';
-import Swiper from 'swiper';
-import 'swiper/css';
+import Swiper from './node_modules/swiper';
 
 const btn = document.querySelector('.btn');
 const footer = document.querySelector('.footer');
@@ -25,3 +24,25 @@ const init = () => {
 };
 
 init();
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
