2238e108b4283498fc8c30c1c52363564bfe68fb
Cheng Lim <wenyong.lim@icloud.com>
2022-10-22 17:03:19 +0100

    removed swiper in js

--- main.js
+++ main.js
@@ -26,31 +26,3 @@ const init = () => {
 };
 
 init();
-
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
-
-  // If we need pagination
-  // pagination: {
-  //   el: '.swiper-pagination',
-  // },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-
-  autoplay: {
-    delay: 2000,
-    // disableOnInteraction: false,
-  },
-  loop: true,
-});
