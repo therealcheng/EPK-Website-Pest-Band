c62d87614670ab8a1839b8165bba3186093134cb
Cheng Lim <wenyong.lim@icloud.com>
2022-11-01 19:46:39 +0000

    Moved items into src folder

---
+++ src/main.js
@@ -0,0 +1,23 @@
+const btn = document.querySelector('.btn');
+const footer = document.querySelector('.footer');
+const navLogo = document.getElementById('#logo');
+
+// const init = () => {
+//   anime({
+//     targets: '.hero',
+//     translateY: 30,
+//     duration: 2000,
+//   });
+//   anime({
+//     targets: '.hero-p',
+//     translateY: 30,
+//     duration: 2000,
+//   });
+//   anime({
+//     targets: '.hero-p2',
+//     translateY: 30,
+//     duration: 2000,
+//   });
+// };
+
+// init();
