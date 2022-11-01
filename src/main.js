b42ad41e49b21021790099458c601bc389033731
Cheng Lim <wenyong.lim@icloud.com>
2022-11-01 22:34:52 +0000

    Removing nearly everything 2

--- src/main.js
+++ src/main.js
@@ -1,7 +1,3 @@
-const btn = document.querySelector('.btn');
-const footer = document.querySelector('.footer');
-const navLogo = document.getElementById('#logo');
-
 // const init = () => {
 //   anime({
 //     targets: '.hero',
