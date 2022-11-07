aee26d50417d86e37dc3c54ba458529b7c2de9f1
Cheng Lim <wenyong.lim@icloud.com>
2022-11-07 18:33:26 +0000

    working logo in react

--- pest-epk/src/components/Nav.jsx
+++ pest-epk/src/components/Nav.jsx
@@ -1,7 +1,30 @@
 import '../styles/Nav.css';
+import logo from '../img/pest-logo.jpg';
 
 const Nav = () => {
-  return <div className='nav'>Nav Bar</div>;
+  return (
+    <div>
+      <nav className='nav' id='background-nav'>
+        <div className='nav-logo'>
+          <img id='logo' src={logo} alt='pest-logo' />
+        </div>
+        <div>
+          <ul className='nav-links'>
+            <li>
+              <a className='nav-link' href='./index.html'>
+                Home
+              </a>
+            </li>
+            <li>
+              <a className='nav-link' href='pestbandofficial@outlook.com'>
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default Nav;
