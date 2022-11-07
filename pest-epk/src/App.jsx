1051900d2daeed427de08e87fcd93361c4688a23
Cheng Lim <wenyong.lim@icloud.com>
2022-11-07 20:08:32 +0000

    hero and about components

--- pest-epk/src/App.jsx
+++ pest-epk/src/App.jsx
@@ -1,8 +1,19 @@
 import Nav from './components/Nav';
 import Hero from './components/Hero';
+import About from './components/About';
 
 function App() {
-  return <Nav />;
+  return (
+    <div>
+      <Nav />
+      <div>
+        <Hero />
+        <div>
+          <About />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- pest-epk/src/components/Hero.jsx
+++ pest-epk/src/components/Hero.jsx
@@ -1,5 +1,14 @@
+import '../styles/Hero.css';
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className='hero-main p-96'>
+      <div className='max-w-md'>
+        <h1 className='mb-10 text-6xl font-bold'>PUNK ROCK TWO PIECE</h1>
+
+        <button className=''>Be ready, this is PEST</button>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
