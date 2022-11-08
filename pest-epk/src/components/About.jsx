84a5f33f85711a3901cf6cb8477b9f1c735d6ec0
FAMILY-PC\wenyo <wenyong.lim@icloud.com>
2022-11-08 17:37:08 +0000

    Updated portrait1.jpg and about text

--- pest-epk/src/components/About.jsx
+++ pest-epk/src/components/About.jsx
@@ -1,16 +1,12 @@
 import '../styles/About.css';
 const About = () => {
   return (
-    <div className=' about'>
-      <h2 className='text-2xl text-black font-bold md:text-4xl uppercase p-12'>
+    <div className='about p-48'>
+      <h2 className='text-2xl text-white font-bold md:text-4xl uppercase p-12'>
         PEST are a punk rock two piece based in Hertfordshire.
       </h2>
-      <p className='mt-6 text-black px-10 text-lg pt-4'>
-        The construction of the energetic unique sound amplified by this duo
-        consists of Powerful Industrial drumming by Harvey Denman and Thunderous
-        Nu-Metal riffs and Punk Rock vocal by Cameron Taylor.
-      </p>
-      <p className='mt-4 text-black px-10 text-lg pt-6'>
+      <p className='mt-6 text-white px-10 text-lg pt-4'></p>
+      <p className='mt-4 text-white px-10 text-lg pt-6'>
         The rebellion started as a Four-Piece in early 2020 but soon took flight
         as a Power Duo leading a new-wave punk rock battle. The PEST boys are
         actively performing shows around London and Hertfordshire with expansion
