da4670041a3d205c2e375aa172c4a4706c054fc3
FAMILY-PC\wenyo <wenyong.lim@icloud.com>
2022-11-08 16:22:42 +0000

    about styling

--- pest-epk/src/components/About.jsx
+++ pest-epk/src/components/About.jsx
@@ -1,24 +1,24 @@
-import logo from '../img/pest-logo.jpg';
+import gif from '../img/pest.gif';
+import '../styles/About.css';
 const About = () => {
   return (
-    <div className='py-16 bg-white'>
+    <div className='py-16 bg-[conic-gradient(at_top_left,_var(--tw-gradient-stops))] from-rose-900 via-amber-800 to-rose-400 '>
       <div className='container m-auto px-6 text-gray-600 md:px-12 xl:px-6'>
         <div className='space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12'>
           <div className='md:5/12 lg:w-5/12'>
-            <img src={logo} alt='pest-band' loading='lazy' width='' height='' />
+            <img src={gif} alt='pest-band' loading='lazy' width='' height='' />
           </div>
           <div className='md:7/12 lg:w-6/12'>
-            <h2 className='text-2xl text-gray-900 font-bold md:text-4xl'>
-              Nuxt development is carried out by passionate developers
+            <h2 className='text-2xl text-black font-bold md:text-4xl'>
+              Pest are a punk rock two piece based in Hertfordshire.
             </h2>
-            <p className='mt-6 text-gray-600'>
+            <p className='mt-6 text-black'>
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum
               omnis voluptatem accusantium nemo perspiciatis delectus atque
               autem! Voluptatum tenetur beatae unde aperiam, repellat expedita
               consequatur! Officiis id consequatur atque doloremque!
             </p>
-            <p className='mt-4 text-gray-600'>
-              {' '}
+            <p className='mt-4 text-black'>
               Nobis minus voluptatibus pariatur dignissimos libero quaerat iure
               expedita at? Asperiores nemo possimus nesciunt dicta veniam
               aspernatur quam mollitia.
