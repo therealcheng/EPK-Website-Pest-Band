bfcff63d7e2cbe9dccad1b029900cd4257630d9c
FAMILY-PC\wenyo <wenyong.lim@icloud.com>
2022-11-08 15:34:48 +0000

    background opacity in Hero

--- pest-epk/src/components/Hero.jsx
+++ pest-epk/src/components/Hero.jsx
@@ -3,7 +3,7 @@ const Hero = () => {
   return (
     <div className='hero-main p-96'>
       <div className='max-w-md'>
-        <h1 className='mb-10 text-6xl font-bold'>PUNK ROCK TWO PIECE</h1>
+        <h1 className='mb-10 text-6xl font-bold'>PUNK ROCK TWO-PIECE</h1>
 
         <button className=''>Be ready, this is PEST</button>
       </div>
