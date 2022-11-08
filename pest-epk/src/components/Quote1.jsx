9e01b758a61f19a20da08bf808721be26113c5e5
FAMILY-PC\wenyo <wenyong.lim@icloud.com>
2022-11-08 22:59:04 +0000

    creating quotes components

---
+++ pest-epk/src/components/Quote1.jsx
@@ -0,0 +1,10 @@
+const Quote1 = () => {
+  return (
+    <div>
+      <h1>“If this does not wake you up, then I don’t know what will.”</h1>
+      <p>Dislocated Magazine</p>
+    </div>
+  );
+};
+
+export default Quote1;
