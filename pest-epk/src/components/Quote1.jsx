6230cb588a3649d41a39711a2b1a249e62b47832
FAMILY-PC\wenyo <wenyong.lim@icloud.com>
2022-11-09 00:21:38 +0000

    quote1

--- pest-epk/src/components/Quote1.jsx
+++ pest-epk/src/components/Quote1.jsx
@@ -1,8 +1,18 @@
 const Quote1 = () => {
   return (
-    <div>
-      <h1>“If this does not wake you up, then I don’t know what will.”</h1>
-      <p>Dislocated Magazine</p>
+    <div className='card w-96 bg-base-100 shadow-xl'>
+      <figure>
+        <img src='https://placeimg.com/400/225/arch' alt='Shoes' />
+      </figure>
+      <div className='card-body'>
+        <h2 className='card-title'>
+          If this does not wake you up, then I don’t know what will
+        </h2>
+
+        <div className='card-actions justify-end'>
+          <button className='btn btn-primary'>Dislocated Magazine</button>
+        </div>
+      </div>
     </div>
   );
 };
--- pest-epk/src/components/Quotes.jsx
+++ pest-epk/src/components/Quotes.jsx
@@ -1,7 +1,7 @@
 import Quote1 from './Quote1';
 
 const Quotes = () => {
-  return <div>{<Quote1 />}</div>;
+  return <div className='p-48'>{<Quote1 />}</div>;
 };
 
 export default Quotes;
