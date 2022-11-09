9eb0147dd1d21b75ef58b5d6cb9c1b5257bc6d17
FAMILY-PC\wenyo <wenyong.lim@icloud.com>
2022-11-09 00:02:44 +0000

    testing miui

--- pest-epk/src/components/Quotes.jsx
+++ pest-epk/src/components/Quotes.jsx
@@ -1,27 +1,7 @@
+// import Quote1 from './Quote1';
+
 const Quotes = () => {
-  return (
-    <div className='bg-blue-700 flex justify-evenly'>
-      <div className='pt-10'>
-        <h1 className='text-white text-xl p-8 max-w-md'>
-          “If this does not wake you up, then I don’t know what will.” <br></br>
-          <p className='flex justify-end text-sm'>Dislocated Magazine</p>
-        </h1>
-        <h1 className='text-white text-xl p-8 max-w-md'>
-          “The heaviest thing to come out of the county since Enter Shikari"
-          <br></br>
-          <p className='flex justify-end text-sm'>
-            Iain Key, Louder Than War Radio
-          </p>
-        </h1>
-        <h1 className='text-white text-xl pt-8 max-w-md'>
-          “I knew nothing about you guys, I walked in and everyone was moshing,
-          it was crazy”
-          <br></br>
-          <p className='flex justify-end text-sm'>Black Feather</p>
-        </h1>
-      </div>
-    </div>
-  );
+  return <div>{/* <Quote1 /> */}</div>;
 };
 
 export default Quotes;
