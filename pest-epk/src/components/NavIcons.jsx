1ff09fd4b50d00b611ec3c17cf2a44ea8eb3c866
Cheng Lim <wenyong.lim@icloud.com>
2022-11-09 16:44:49 +0000

    removing Nav banner image

---
+++ pest-epk/src/components/NavIcons.jsx
@@ -0,0 +1,18 @@
+import '../styles/Nav.css';
+
+const NavIcons = () => {
+  return (
+    <div>
+      <ul></ul>
+      <ul></ul>
+      <ul></ul>
+      <ul></ul>
+      <ul></ul>
+      <ul></ul>
+      <ul></ul>
+      <ul></ul>
+    </div>
+  );
+};
+
+export default NavIcons;
