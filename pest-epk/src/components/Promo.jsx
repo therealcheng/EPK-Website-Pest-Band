755ce718d053fe5e166c9145786d5278a6e4dfaa
Cheng Lim <wenyong.lim@icloud.com>
2022-11-09 16:36:47 +0000

    downloadable Press Photos (PromoDownloads)

--- pest-epk/src/components/Promo.jsx
+++ pest-epk/src/components/Promo.jsx
@@ -1,13 +1,17 @@
 import '../styles/Promo.css';
 import Carousel from './Carousel';
+import PromoDownloads from './PromoDownloads';
 
 const Promo = () => {
   return (
-    <div className='background flex justify-center align-middle'>
-      <div className='p-48 text-6xl text-white'>
-        PRESS PHOTOS
+    <div className='background'>
+      <div className='p-48'>
+        <h1 className=' text-6xl text-white'>PRESS PHOTOS</h1>
         <div className='p-48 flex gap-6'>
           <Carousel />
+        </div>
+        <div>
+          <PromoDownloads />
         </div>
       </div>
     </div>
--- pest-epk/src/components/PromoDownloads.jsx
+++ pest-epk/src/components/PromoDownloads.jsx
@@ -1,8 +1,53 @@
+import image1 from '../img/promo/01.jpg';
+import image2 from '../img/promo/02.jpg';
+import image3 from '../img/promo/03.jpg';
+import image4 from '../img/promo/04.jpg';
+import image5 from '../img/promo/05.jpg';
+import image6 from '../img/promo/06.jpg';
+
 const PromoDownloads = () => {
   return (
     <div>
-      <div>
-        <h1>Downloads</h1>
+      <div className='flex-col justify-center text-white text-2xl'>
+        <ul>
+          <h1 className='text-3xl underline'>Jpegs</h1>
+        </ul>
+        <ul>
+          Download &nbsp;
+          <a className='text-red-500' href={image1} download>
+            Image 1
+          </a>
+        </ul>
+        <ul>
+          Download &nbsp;
+          <a className='text-red-500' href={image2} download>
+            Image 2
+          </a>
+        </ul>
+        <ul>
+          Download &nbsp;
+          <a className='text-red-500' href={image3} download>
+            Image 3
+          </a>
+        </ul>
+        <ul>
+          Download &nbsp;
+          <a className='text-red-500' href={image4} download>
+            Image 4
+          </a>
+        </ul>
+        <ul>
+          Download &nbsp;
+          <a className='text-red-500' href={image5} download>
+            Image 5
+          </a>
+        </ul>
+        <ul>
+          Download &nbsp;
+          <a className='text-red-500' href={image6} download>
+            Image 6
+          </a>
+        </ul>
       </div>
     </div>
   );
