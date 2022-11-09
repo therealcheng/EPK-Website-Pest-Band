e20c98bd676a80c3d5246eef3231713fb8fe6192
FAMILY-PC\wenyo <wenyong.lim@icloud.com>
2022-11-09 00:06:25 +0000

    removed Mui

--- pest-epk/src/components/Quotes.jsx
+++ pest-epk/src/components/Quotes.jsx
@@ -1,7 +1,7 @@
-// import Quote1 from './Quote1';
+import Quote1 from './Quote1';
 
 const Quotes = () => {
-  return <div>{/* <Quote1 /> */}</div>;
+  return <div>{<Quote1 />}</div>;
 };
 
 export default Quotes;
