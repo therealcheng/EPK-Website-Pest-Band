5feea72a6ede76f0d465efdbd1ff9f0faf28255c
Cheng Lim <wenyong.lim@icloud.com>
2022-11-10 11:19:22 +0000

    About section Mobile

--- pest-epk/src/components/About.jsx
+++ pest-epk/src/components/About.jsx
@@ -1,12 +1,11 @@
 import '../styles/About.css';
 const About = () => {
   return (
-    <div className='about p-96'>
-      <h2 className='text-2xl text-white font-bold md:text-4xl uppercase p-12'>
+    <div className='p-24 background-img lg:about lg:p-96'>
+      <h2 className='text-white font-bold lg:pb-40 pl-8 lg:text-4xl uppercase   lg:p-12'>
         PEST are a punk rock two piece based in Hertfordshire.
       </h2>
-      <p className='mt-6 text-white px-10 text-lg pt-4'></p>
-      <p className='mt-4 text-white px-10 text-lg pt-6'>
+      <p className=' text-white text-sm mt-4 lg:text-white px-10 lg:text-lg'>
         PEST is an Industrial Nu-Punk Two-Piece movement from the UK. The
         energetic unique sound amplified by this duo consists of Powerful
         Industrial drumming by Harvey Denman and Thunderous Nu-Metal riffs and
