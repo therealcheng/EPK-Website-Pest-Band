113281be40dd5f5ac7a5e89f890d4fc9f099bd99
Cheng Lim <wenyong.lim@icloud.com>
2022-11-10 20:27:05 +0000

    Hero font change

--- pest-epk/src/components/Hero.jsx
+++ pest-epk/src/components/Hero.jsx
@@ -4,10 +4,10 @@ const Hero = () => {
   return (
     <div className='hero-main lg:p-96'>
       <div className='lg:block lg:max-w-md'>
-        <h1 className='text-2xl flex lg:block pr-12 justify-end p-48 lg:mb-10 lg:text-8xl lg:font-bold lg:pr-0 lg:pl-96'>
+        <h1 className='text-2xl flex lg:block pr-12 justify-end p-48 lg:mb-10 lg:text-6xl lg:font-bold lg:pr-0 lg:pl-96'>
           IN PURGATORY <br></br>
         </h1>
-        <h1 className='flex lg:justify-end justify-center text-2xl lg:mb-10 lg:text-7xl font-bold'>
+        <h1 className='flex lg:justify-end justify-center text-2xl lg:mb-10 lg:text-xl font-bold'>
           DECEMBER 3rd
         </h1>
         {/* <button className=''>Be ready, this is PEST</button> */}
