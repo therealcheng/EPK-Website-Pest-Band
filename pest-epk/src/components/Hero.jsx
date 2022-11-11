2976230d3e9bbec629b91ab6cf938757b3677002
Cheng Lim <wenyong.lim@icloud.com>
2022-11-11 22:48:54 +0000

    Fade in for Hero

--- pest-epk/src/components/Hero.jsx
+++ pest-epk/src/components/Hero.jsx
@@ -9,7 +9,12 @@ const Hero = () => {
     console.log('works');
   };
   return (
-    <div className='hero-main lg:p-96'>
+    <motion.div
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0.1 }}
+      transition={{ duration: 8 }}
+      className='hero-main lg:p-96'
+    >
       <div className='lg:block lg:max-w-md'>
         <h1 className='text-2xl flex lg:block pr-12 justify-end p-48 lg:mb-10 lg:text-6xl lg:font-bold lg:pr-0 lg:pl-96'>
           IN PURGATORY
@@ -28,7 +33,7 @@ const Hero = () => {
       <h1 className='flex lg:justify-end justify-center text-2xl lg:mb-10 lg:text-xl font-bold'>
         RELEASE DATE: 03/12/2022
       </h1>
-    </div>
+    </motion.div>
   );
 };
 
