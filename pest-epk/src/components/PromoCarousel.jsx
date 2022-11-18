8fb7d223c0014aa97742a66f45d72594071c34ae
FAMILY-PC\wenyo <wenyong.lim@icloud.com>
2022-11-18 23:11:26 +0000

    Zoom motion for PromoCarousel

--- pest-epk/src/components/PromoCarousel.jsx
+++ pest-epk/src/components/PromoCarousel.jsx
@@ -7,28 +7,53 @@ import image6 from '../img/promo/06.jpeg';
 import '../styles/Promo.css';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
+import { motion } from 'framer-motion';
 
 const Carousel = () => {
   return (
     <div className='carousel rounded-box'>
-      <div className='carousel-item w-60 lg:w-96'>
+      <motion.div
+        whileHover={{ scale: 1.1 }}
+        transition={{ duration: 0.3 }}
+        className='carousel-item w-60 lg:w-96'
+      >
         <LazyLoadImage src={image1} alt='Press1' />
-      </div>
-      <div className='carousel-item w-60 lg:w-96'>
+      </motion.div>
+      <motion.div
+        whileHover={{ scale: 1.1 }}
+        transition={{ duration: 0.3 }}
+        className='carousel-item w-60 lg:w-96'
+      >
         <LazyLoadImage src={image2} alt='Press2' />
-      </div>
-      <div className='carousel-item w-60 lg:w-96'>
+      </motion.div>
+      <motion.div
+        whileHover={{ scale: 1.1 }}
+        transition={{ duration: 0.3 }}
+        className='carousel-item w-60 lg:w-96'
+      >
         <LazyLoadImage src={image3} alt='Press3' />
-      </div>
-      <div className='carousel-item w-60 lg:w-96'>
+      </motion.div>
+      <motion.div
+        whileHover={{ scale: 1.1 }}
+        transition={{ duration: 0.3 }}
+        className='carousel-item w-60 lg:w-96'
+      >
         <LazyLoadImage src={image4} alt='Press4' />
-      </div>
-      <div className='carousel-item w-60 lg:w-96'>
+      </motion.div>
+      <motion.div
+        whileHover={{ scale: 1.1 }}
+        transition={{ duration: 0.3 }}
+        className='carousel-item w-60 lg:w-96'
+      >
         <LazyLoadImage src={image5} alt='Press5' />
-      </div>
-      <div className='carousel-item w-60 lg:w-96'>
+      </motion.div>
+      <motion.div
+        whileHover={{ scale: 1.1 }}
+        transition={{ duration: 0.3 }}
+        className='carousel-item w-60 lg:w-96'
+      >
         <LazyLoadImage src={image6} alt='Press6' />
-      </div>
+      </motion.div>
     </div>
   );
 };
