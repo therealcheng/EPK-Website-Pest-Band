36c56c0b2f8f3e5e989fbea6e780d4ab4ccd7f21
Cheng Lim <wenyong.lim@icloud.com>
2022-11-29 16:22:37 +0000

    Refactored Qutoe 2 and 3

--- pest-epk/src/components/Quote2.jsx
+++ pest-epk/src/components/Quote2.jsx
@@ -1,43 +1,37 @@
 import image from '../img/louderThanWar.png';
 import { motion } from 'framer-motion';
 
-const Quote2 = () => {
-  const link = (e) => {
-    e.preventDefault();
-    window.open(
-      'https://louderthanwar.com/pest-nicotine-wake-up-single-review/'
-    );
-  };
+const cite1 = {
+  name: 'Iain Key, Louder Than War Radio',
+  quote: ' “The heaviest thing to come out of the county since Enter Shikari”',
+  link: 'https://louderthanwar.com/pest-nicotine-wake-up-single-review/',
+};
+
+const Quote1 = () => {
   return (
-    <div className='card  w-72 lg:w-96 bg-base-100 shadow-xl'>
+    <div className='card w-72 lg:w-96 bg-base-100 shadow-xl'>
       <figure>
-        <a
-          href='https://louderthanwar.com/pest-nicotine-wake-up-single-review/'
-          target='_blank'
-          rel='noreferrer'
-        >
+        <a href={cite1.link} target='_blank' rel='noreferrer'>
           <motion.img
             whileHover={{ scale: 1.1, opacity: 0.9 }}
             transition={{ duration: 0.3 }}
             className='hidden md:block lg:block'
             src={image}
-            alt='Louder Than War'
+            alt='Article Image'
           />
         </a>
       </figure>
       <div className='card-body'>
-        <h2 className='italic card-title text-white'>
-          “The heaviest thing to come out of the county since Enter Shikari”
-        </h2>
-        <p>Iain Key, Louder Than War Radio</p>
+        <h2 className='italic card-title text-white'>{cite1.quote}</h2>
+        <p>{cite1.name}</p>
         <div className='card-actions justify-end'>
-          <button className='btn btn-primary' onClick={link}>
-            Read Article
-          </button>
+          <form action={cite1.link} target='_blank'>
+            <button className='btn btn-primary'>Read Article</button>
+          </form>
         </div>
       </div>
     </div>
   );
 };
 
-export default Quote2;
+export default Quote1;
--- pest-epk/src/components/Quote3.jsx
+++ pest-epk/src/components/Quote3.jsx
@@ -1,40 +1,38 @@
 import image from '../img/blackFeather.png';
 import { motion } from 'framer-motion';
 
-const Quote3 = () => {
-  const link = (e) => {
-    e.preventDefault();
-    window.open('https://vm.tiktok.com/ZMFPRDH5j/');
-  };
+const cite1 = {
+  name: 'Black Feather',
+  quote: `“I knew nothing about you guys, I walked in and everyone was moshing,
+  it was crazy"`,
+  link: 'https://vm.tiktok.com/ZMFPRDH5j/',
+};
+
+const Quote1 = () => {
   return (
     <div className='card w-72 lg:w-96 bg-base-100 shadow-xl'>
       <figure>
-        <a
-          href='https://vm.tiktok.com/ZMFPRDH5j/'
-          target='_blank'
-          rel='noreferrer'
-        >
+        <a href={cite1.link} target='_blank' rel='noreferrer'>
           <motion.img
             whileHover={{ scale: 1.1, opacity: 0.9 }}
             transition={{ duration: 0.3 }}
             className='hidden md:block lg:block'
             src={image}
-            alt='Tiktok id:blackfeatherblackfeather'
+            alt='Article Image'
           />
         </a>
       </figure>
       <div className='card-body'>
-        <h2 className='italic card-title text-white'>
-          “I knew nothing about you guys, I walked in and everyone was moshing,
-          it was crazy"
-        </h2>
-        <p>Black Feather</p>
-        <div className='card-actions justify-end' onClick={link}>
-          <button className='btn btn-primary'>Watch Interview</button>
+        <h2 className='italic card-title text-white'>{cite1.quote}</h2>
+        <p>{cite1.name}</p>
+        <div className='card-actions justify-end'>
+          <form action={cite1.link} target='_blank'>
+            <button className='btn btn-primary'>Read Article</button>
+          </form>
         </div>
       </div>
     </div>
   );
 };
 
-export default Quote3;
+export default Quote1;
