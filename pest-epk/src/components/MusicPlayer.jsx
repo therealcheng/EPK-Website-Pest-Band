11e30c44196ef40aa16e3d1d7fc3f6b6cfde3cb1
PACMAN-3\wenyo <wenyong.lim@icloud.com>
2022-12-22 19:19:48 +0000

    React Music player ideas (comment)

--- pest-epk/src/components/MusicPlayer.jsx
+++ pest-epk/src/components/MusicPlayer.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const MusicPlayer = () => {
-  return (
-    <div>MusicPlayer</div>
-  )
-}
+  return <div>MusicPlayer</div>;
+};
 
-export default MusicPlayer
+export default MusicPlayer;
+
+// React Music Player Ideas
+//https://www.npmjs.com/package/react-jinke-music-player
+// https://freefrontend.com/react-music-players/
