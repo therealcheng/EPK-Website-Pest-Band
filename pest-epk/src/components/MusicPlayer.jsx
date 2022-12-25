ff001489b34ef1b925b0f8b28235e8f3465b26db
PACMAN-3\wenyo <wenyong.lim@icloud.com>
2022-12-25 21:40:14 +0000

    Import all mp3 to be used

--- pest-epk/src/components/MusicPlayer.jsx
+++ pest-epk/src/components/MusicPlayer.jsx
@@ -1,12 +1,14 @@
 import ReactAudioPlayer from 'react-audio-player';
 import safe from '../music/Safe.mp3';
+import step from '../music/Step.mp3';
+import www from '../music/WWW.mp3';
 
 const MusicPlayer = () => {
-  return <ReactAudioPlayer src={safe} autoPlay controls />;
+  return (
+    <div className='place-self-auto'>
+      <ReactAudioPlayer src={safe} autoPlay controls />
+    </div>
+  );
 };
 
 export default MusicPlayer;
-
-// React Music Player Ideas
-//https://www.npmjs.com/package/react-jinke-music-player
-// https://freefrontend.com/react-music-players/
