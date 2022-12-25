e676c94a142eb50cc5554f2ef8ab5b730fb5217f
PACMAN-3\wenyo <wenyong.lim@icloud.com>
2022-12-25 19:40:22 +0000

    Revert to previous commit

--- pest-epk/src/components/PromoCarousel.jsx
+++ pest-epk/src/components/PromoCarousel.jsx
@@ -8,15 +8,8 @@ import '../styles/Promo.css';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 import { motion } from 'framer-motion';
-import React, { useState } from 'react';
 
 const Carousel = ({ src }) => {
-  const [fullScreen, setFullScreen] = useState(false);
-
-  const toggleFullScreen = () => {
-    setFullScreen(!fullScreen);
-  };
-
   return (
     <div className='carousel rounded-box'>
       <motion.div
@@ -24,15 +17,7 @@ const Carousel = ({ src }) => {
         transition={{ duration: 0.3 }}
         className='carousel-item w-60 lg:w-96'
       >
-        <LazyLoadImage
-          src={image1}
-          onClick={toggleFullScreen}
-          style={{
-            width: fullScreen ? '150%' : '50%',
-            height: fullScreen ? '100%' : '50%',
-          }}
-          alt='Press1'
-        />
+        <LazyLoadImage src={image1} alt='Press1' />
       </motion.div>
       <motion.div
         whileHover={{ scale: 1.1 }}
