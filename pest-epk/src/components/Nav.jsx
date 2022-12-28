8ee016f402a125b94e4bf069fffe98ff7ba1a838
PACMAN-3\wenyo <wenyong.lim@icloud.com>
2022-12-28 17:53:29 +0000

    Refactoring Nav-Bar to remove boilerplate

--- pest-epk/src/components/Nav.jsx
+++ pest-epk/src/components/Nav.jsx
@@ -5,34 +5,17 @@ import IconLinks from './IconLinks';
 const Nav = () => {
   return (
     <div>
-      <nav className='nav lg:flex lg:gap-12' id='background-nav'>
+      <section className='nav lg:flex lg:gap-12' id='background-nav'>
         <div className='flex justify-center'>
           <img className='lg:hidden' src={logo} width={50} alt='pest-logo' />
         </div>
         <div className='nav-logo'>
           <img className='hidden lg:block w-20' src={logo} alt='pest-logo' />
-          <div className='lg:pl-12'>
+          <div className='lg:pl-12 lg:pt-8 pb-8'>
             <IconLinks />
           </div>
         </div>
-        <div className='pt-8'>
-          <ul className='lg:nav-links'>
-            <li>
-              {/* <a
-                className='hidden md:flex md:nav-link md:justify-center md:pb-8'
-                href='./index.html'
-              >
-                Electronic Press Kit
-              </a> */}
-            </li>
-            {/* <li>
-              <a className='nav-link' href='pestbandofficial@outlook.com'>
-                Contact
-              </a>
-            </li> */}
-          </ul>
-        </div>
-      </nav>
+      </section>
     </div>
   );
 };
