cde97585824fb9de0b4420423608bab717172ba4
PACMAN-3\wenyo <wenyong.lim@icloud.com>
2023-01-27 14:43:24 +0000

    Changing padding in About

--- pest-epk-next/components/About.jsx
+++ pest-epk-next/components/About.jsx
@@ -1,6 +1,6 @@
 const About = () => {
   return (
-    <div className='p-4 py-36 lg:about lg:p-12 bg-[conic-gradient(at_top_right,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black'>
+    <div className='p-4 py-24 lg:about lg:p-12 bg-[conic-gradient(at_top_right,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black'>
       <h2 className='text-white font-bold lg:pb-2 pl-8 lg:text-4xl uppercase lg:p-12'>
         PEST are a punk rock two piece based in Hertfordshire.
       </h2>
