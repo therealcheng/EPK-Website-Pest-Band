4f84581ce0327b307e88f89fa77a6f705169be62
Cheng Lim <wenyong.lim@icloud.com>
2023-02-16 22:39:15 +0000

    Changing About (Bio)

--- pest-epk-next/components/About.jsx
+++ pest-epk-next/components/About.jsx
@@ -1,16 +1,40 @@
 const About = () => {
   return (
     <div className='p-4 py-24 lg:about lg:p-12 bg-[conic-gradient(at_top_right,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black'>
-      <h2 className='text-white font-bold lg:pb-2 pl-8 lg:text-4xl uppercase lg:p-12'>
+      {/* <h2 className='text-white font-bold lg:pb-2 pl-8 lg:text-4xl uppercase lg:p-12'>
         PEST are a punk rock two piece based in Hertfordshire.
-      </h2>
+      </h2> */}
       <p className=' text-white text-md mt-4 lg:text-white px-10 lg:text-lg'>
-        PEST is an Industrial Nu-Punk Two-Piece movement from the UK. The
-        energetic unique sound amplified by this duo consists of Powerful
-        Industrial drumming by Harvey Denman and Thunderous Nu-Metal riffs and
-        Punk Rock vocals by Cameron Taylor. The rebellion started in early 2020
-        and took flight in London as a Power Duo leading a new-wave punk rock
-        battle.
+        PEST, a two-person punk rock band, was formed in Hertfordshire, a county
+        in southern England, in early 2020. The band's name is an acronym for
+        "Powerful Electric Shock Therapy," a name that reflects their energetic
+        and rebellious approach to music. The band consists of two talented
+        musicians, Harvey Denman and Cameron Taylor, who have been playing music
+        together for several years.
+      </p>
+      <p className=' text-white text-md mt-4 lg:text-white px-10 lg:text-lg'>
+        The duo's music can be described as a unique blend of Punk and
+        Industrial Nu-Metal genres. Harvey's powerful and dynamic drumming,
+        combined with Cameron's thunderous guitar riffs and punk rock vocals,
+        creates a sound that is both powerful and captivating. Their energetic
+        performances are a true reflection of the DIY punk rock ethos, and they
+        have quickly established themselves as one of the most exciting
+        up-and-coming bands in the UK music scene.
+      </p>
+      <p className=' text-white text-md mt-4 lg:text-white px-10 lg:text-lg'>
+        In early 2020, PEST started to gain a following, performing at local
+        venues in Hertfordshire and the surrounding areas. However, it wasn't
+        long before their music caught the attention of the wider music
+        community in London. The duo's power-packed performances and their
+        unique sound soon led to them being recognized as a Power Duo leading a
+        new-wave punk rock battle.
+      </p>
+      <p className=' text-white text-md mt-4 lg:text-white px-10 lg:text-lg'>
+        Since then, PEST has continued to make waves in the UK music scene.
+        Their music has been well-received by both fans and critics, and they
+        have been featured in various music publications and online platforms.
+        With their raw energy and unique style, PEST is a band to watch out for
+        in the coming years.
       </p>
     </div>
   );
